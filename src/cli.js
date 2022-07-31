23f76b1ed5058cd6688fa220bfccc86aea4c556b
runabal <nvbalandina@mail.ru>
2022-07-31 15:13:30 +0300

    src

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const hello = () => {
+  const userName = readlineSync.question('May I have you name? ');
+  const result = `Hello, ${userName}!`;
+  return result;
+};
+export default hello;
