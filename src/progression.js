78f50816e5146038d89ad27c089a9b82ea1d0a50
runabal <nvbalandina@mail.ru>
2022-09-05 01:59:01 +0300

    update progression.js

--- src/progression.js
+++ src/progression.js
@@ -23,8 +23,7 @@ const progression = () => {
       console.log("Correct!");
       i += 1;
     } else {
-      console.log(userAnswer + " is wrong answer ;(. Correct answer was " + result[hiddenNumber] + ".");
-      console.log("Let's try again, " + userName + "!");
+      console.log(userAnswer + " is wrong answer ;(. Correct answer was " + result[hiddenNumber] + ". Let's try again, " + userName + "!");
       break;
     }
   } while (i < 3);
