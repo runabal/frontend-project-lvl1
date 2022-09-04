4158189b79edbce4dccc48040b4be29fb050b7f0
runabal <nvbalandina@mail.ru>
2022-09-05 01:20:42 +0300

    update gcd.js

--- src/gcd.js
+++ src/gcd.js
@@ -2,14 +2,13 @@ import readlineSync from "readline-sync";
 
 const gcd = () => {
   const userName = readlineSync.question("May I have your name? ");
-  console.log("Hello ", `${+userName}!`);
+  console.log("Hello, ", `${userName}!`);
   console.log("Find the greatest common divisor of given numbers. ");
   let i = 0;
   do {
     const num1 = Math.floor(Math.random() * 100) + 1;
     const num2 = Math.floor(Math.random() * 100) + 1;
-    console.log("Question: ");
-    console.log(num1, num2);
+    console.log('Question: ' + num1 + ' ' + num2);
     let result = num1;
     while (num1 % result !== 0 || num2 % result !== 0) {
       result -= 1;
@@ -27,7 +26,7 @@ const gcd = () => {
   } while (i < 3);
 
   if (i === 3) {
-    console.log(`Congratulation, ${userName}!`);
+    console.log(`Congratulations, ${userName}!`);
   }
 };
 export default gcd;
