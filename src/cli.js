47004502b6d422e80c217cfc8beca84c3e496d21
runabal <nvbalandina@mail.ru>
2022-09-05 00:30:27 +0300

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readlineSync from "readline-sync";
 
 const hello = () => {
   console.log('Welcome to the Brain Games!');
+  console.log ('           ');
   const userName = readlineSync.question('May I have you name? ');
   const result = `Hello, ${userName}!`;
   return result;
