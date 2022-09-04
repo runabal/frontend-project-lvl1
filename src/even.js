e0b5ff726c80d74adb52eaf88671fa755def7c41
runabal <nvbalandina@mail.ru>
2022-09-05 00:38:37 +0300

    update even.js

--- src/even.js
+++ src/even.js
@@ -1,7 +1,7 @@
 import readlineSync from "readline-sync";
 
 const even = () => {
-  const userName = readlineSync.question("May I have you name? ");
+  const userName = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let i = 0;
