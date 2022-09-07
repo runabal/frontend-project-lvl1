4b648d1285f33c77bf7a528239bfc0f5701d99be
runabal <nvbalandina@mail.ru>
2022-09-07 10:28:27 +0300

    update progression.js

--- src/progression.js
+++ src/progression.js
@@ -24,7 +24,7 @@ const progression = () => {
       console.log('Correct!');
       i += 1;
     } else {
-      console.log(`${userAnswer}is wrong answer ;(. Correct answer was ${resulthiddenNumber}.`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${resulthiddenNumber}.`);
       console.log(`Let's try again, ${userName}!`);
       break;
     }
