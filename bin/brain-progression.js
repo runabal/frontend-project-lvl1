5154998d52e7ef69d3f3eaf499739d5b13ecf5fc
runabal <nvbalandina@mail.ru>
2022-09-07 10:57:35 +0300

    update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import progression from "../src/progression.js";
+import progression from '../src/progression.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 console.log(progression());
